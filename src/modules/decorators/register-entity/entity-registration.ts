9c25fc4b14494c532805702f2df4e6d317b269db
Josh Gibson <gibsonjoshua55@gmail.com>
2019-07-18 09:19:01 -0400

    fix: remove console log on registration

--- src/modules/decorators/register-entity/entity-registration.ts
+++ src/modules/decorators/register-entity/entity-registration.ts
@@ -4,8 +4,6 @@ export class EntityRegistration {
     private static models: (typeof Model)[] = [];
 
     static registerEntity(model: typeof Model) {
-        // tslint:disable-next-line: no-console
-        console.log(`registering entity: ${model.name}`);
         EntityRegistration.models.push(model);
     }
 

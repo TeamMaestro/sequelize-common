cb84865f023147cba1385b161d511f4669548f78
Josh Gibson <gibsonjoshua55@gmail.com>
2019-07-19 12:50:35 -0400

    feat(types): Adds SequelizeFunctionType and deprecates SequelizeDate

--- src/modules/types/sequelize-date.type.ts
+++ src/modules/types/sequelize-date.type.ts
@@ -1,3 +1,6 @@
 import { Fn } from 'sequelize/types/lib/utils';
 
+/**
+ * @deprecated Use SequelizeFunctionType<Date> instead
+ */
 export type SequelizeDate = Date | Fn;
---
+++ src/modules/types/sequelize-function-type.type.ts
@@ -0,0 +1,3 @@
+import { Fn } from 'sequelize/types/lib/utils';
+
+export type SequelizeFunctionType<T> = T | Fn;

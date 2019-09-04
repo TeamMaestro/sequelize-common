0adc22d48fb46c1381389c5c13f60f7c7cab9882
Josh Gibson <gibsonjoshua55@gmail.com>
2019-09-04 13:24:20 -0400

    chore: update to sequelize 1.0.0 and other dependencies

--- src/modules/decorators/register-entity/register-entity.decorator.ts
+++ src/modules/decorators/register-entity/register-entity.decorator.ts
@@ -1,9 +1,8 @@
-import { Model } from 'sequelize-typescript';
+import { ModelCtor } from 'sequelize-typescript';
 import { EntityRegistration } from './entity-registration';
 
-// tslint:disable-next-line: ban-types
 export function RegisterEntity(): ClassDecorator {
     return (target) => {
-        EntityRegistration.registerEntity(target as unknown as typeof Model);
+        EntityRegistration.registerEntity(target as unknown as ModelCtor<any>);
     };
 }

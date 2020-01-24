ac44fdbc3e58dc4d646116a3702bc47c34a85178
John Pinkster <john.c.pinkster@gmail.com>
2020-01-24 17:56:49 -0500

    feat: allowing sequelize date to be a string

--- src/modules/types/sequelize-date.type.ts
+++ src/modules/types/sequelize-date.type.ts
@@ -3,4 +3,4 @@ import { Fn } from 'sequelize/types/lib/utils';
 /**
  * @deprecated Use SequelizeFunctionType<Date> instead
  */
-export type SequelizeDate = Date | Fn;
+export type SequelizeDate = Date | string | Fn;

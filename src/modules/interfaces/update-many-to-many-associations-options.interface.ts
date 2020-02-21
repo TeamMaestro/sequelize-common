6e0bfe49057f0dc73690dcfb635ec0ed6b1a88f2
Josh Gibson <gibsonjoshua55@gmail.com>
2020-02-21 16:13:26 -0500

    feat(update-m2m): refactors update many to many and one to many

--- src/modules/interfaces/update-many-to-many-associations-options.interface.ts
+++ src/modules/interfaces/update-many-to-many-associations-options.interface.ts
@@ -1,13 +1,18 @@
-import { Transaction } from 'sequelize';
 import { ModelCtor } from 'sequelize-typescript';
-import { InstanceSpecificJoinTableField } from './instance-specific-join-table-field.interface';
+import { AuthenticatedUser } from './authenticated-user.interface';
+import { UpdateOneToManyAssociationsOptions } from './update-one-to-many-associations-options.interface';
 import { CreatedByEntity } from '../models/created-by.entity';
 import { JoinTableEntity } from '../models/join-table.entity';
 import { AttributesOf } from '../types';
 
 export interface UpdateManyToManyAssociationsOptions<
-    T extends JoinTableEntity | CreatedByEntity<T>
-> {
+    T extends JoinTableEntity | CreatedByEntity<T>,
+    AuthenticatedUserType extends AuthenticatedUser = AuthenticatedUser,
+    NewChildrenType = any
+    > extends
+    Omit<
+    Omit<UpdateOneToManyAssociationsOptions<T, AuthenticatedUserType, NewChildrenType>, 'childTableModel'>,
+    'currentChildren'> {
     /**
      * The id of the parent instance
      */
@@ -21,52 +26,9 @@ export interface UpdateManyToManyAssociationsOptions<
      */
     parentForeignKey: keyof AttributesOf<T>;
     /**
-     * The column name for the foreign on the join table for the child
-     */
-    childForeignKey: keyof AttributesOf<T>;
-    /**
-     * The new child with the that has the parentForeignKey, childForeignKey,
-     * possibly sort order, and additional fields for the join table.
-     */
-    newChildren: any[];
-    /**
-     * The user updating the association
-     */
-    updatingUserId: number;
-    /**
-     * Fields that are created/updated in on the join table based on their
-     * values on the child.
-     */
-    childCreateOrUpdateFields?: string[];
-    /**
-     * The key on the children object to be used as the child primary key
-     */
-    childPrimaryKey?: string;
-    /**
-     * The keys used to determine if the child is a duplicate or if it already exists.
-     * Useful when multiple properties on the child determine uniqueness.
-     */
-    childComparisonKeys?: string[];
-    /**
      * Attributes to request from the database.  These are used when determining
      * if the child exists and to what parent it is related.  Should be given
      * if default scope does not include enough information.
      */
-    joinTableFindAttributes?: string[];
-    /**
-     * The transaction to run the update in
-     */
-    transaction?: Transaction;
-    /**
-     * If true, sort order will be examined and updated on the child
-     */
-    hasSortOrder?: boolean;
-    /**
-     *
-     */
-    instanceSpecificJoinTableFields?: InstanceSpecificJoinTableField[];
-    /**
-     * These fields will be added to all children objects when created.
-     */
-    additionalJoinTableCreateFields?: any;
+    joinTableFindAttributes: (keyof AttributesOf<T>)[];
 }
--- src/modules/utils/index.ts
+++ src/modules/utils/index.ts
@@ -1,3 +1,4 @@
 export * from './get-attributes.util';
-export * from './update-many-to-many-associations.util';
 export * from './update-one-to-many-associations.util';
+export * from './get-search-where-clause.util';
+export * from './update-many-to-many-associations.util';

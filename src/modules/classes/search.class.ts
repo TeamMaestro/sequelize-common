57fb2c1e3143a84d4b845fa9f61c9a9dff08015e
Josh Gibson <gibsonjoshua55@gmail.com>
2020-02-21 16:14:03 -0500

    feat(search): exports function to get search where clause

--- src/modules/classes/search.class.ts
+++ src/modules/classes/search.class.ts
@@ -1,21 +1,12 @@
-import { Op } from 'sequelize';
 import { Model } from 'sequelize-typescript';
+import { getSearchWhereClause } from '../utils';
 
 export class Search {
     include: any;
 
     constructor(model: typeof Model, searchTerms: string = '', alias: string = 'search') {
         if (searchTerms.length > 0) {
-            const whereClause = [];
-            const searchSplit = searchTerms.split(' ');
-            searchSplit.forEach(term => {
-                whereClause.push({
-                    // TODO: Why does this have to be underscored??
-                    search_term: {
-                        [Op.iLike]: `%${term}%`
-                    }
-                });
-            });
+            const whereClause = getSearchWhereClause(searchTerms);
             this.include = {
                 model,
                 as: alias,

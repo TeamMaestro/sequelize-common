568ff4987cc1a4f1d67f70a063495488fa40c679
Josh Gibson <gibsonjoshua55@gmail.com>
2020-02-24 23:38:55 -0500

    feat(updatem2m): add default comparison function

--- src/modules/utils/index.ts
+++ src/modules/utils/index.ts
@@ -3,3 +3,4 @@ export * from './update-one-to-many-associations.util';
 export * from './get-search-where-clause.util';
 export * from './update-many-to-many-associations.util';
 export * from './default-update-many-to-many-fill-function.util';
+export * from './default-update-association-comparison-function.util';

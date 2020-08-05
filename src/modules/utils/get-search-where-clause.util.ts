46d9ca7f3c8c762a8728f4bdcc849f0a91dea50f
John Pinkster <john.c.pinkster@gmail.com>
2020-08-05 01:13:31 -0400

    feat(search): preserve quoted terms

--- src/modules/utils/get-search-where-clause.util.ts
+++ src/modules/utils/get-search-where-clause.util.ts
@@ -3,17 +3,30 @@ import { Op, WhereOptions } from 'sequelize';
 export function getSearchWhereClause(searchTerms = ''): WhereOptions {
     const whereClauses = [];
     if (searchTerms.length > 0) {
-        const searchSplit = searchTerms.split(' ');
+        const regEx = /[^\s"]+|"([^"]*)"/gi;
+        const searchSplit = [];
+
+        let match: RegExpExecArray;
+        do {
+            // Each call to exec returns the next regex match as an array
+            match = regEx.exec(searchTerms);
+            if (match != null) {
+                // Index 1 in the array is the captured group if it exists
+                // Index 0 is the matched text, which we use if no captured group exists
+                searchSplit.push(match[1] ? match[1] : match[0]);
+            }
+        } while (match != null);
+
         searchSplit.forEach(term => {
             whereClauses.push({
                 // TODO: Why does this have to be underscored??
                 search_term: {
-                    [Op.iLike]: `%${term}%`
-                }
+                    [Op.iLike]: `%${term}%`,
+                },
             });
         });
     }
     return {
-        [Op.and]: whereClauses
+        [Op.and]: whereClauses,
     };
 }
